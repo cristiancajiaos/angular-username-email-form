9e3a9cb1441b0cac376d015042e8fd158eea45c7
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2022-02-02 12:53:46 -0300

    Implementado formulario reactivo

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-layout',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
+  public myForm: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email])
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public isFieldInvalid(field: string): boolean {
+    let control = this.myForm.controls[field];
+    return control.touched && control.invalid;
+  }
+
+  public isFieldRequiredError(field: string): boolean {
+    let control = this.myForm.controls[field];
+    return control.touched && control.errors['required'];
+  }
+
+  public isFieldEmailError(): boolean {
+    let control = this.myForm.controls['email'];
+    return control.touched && control.errors['email'];
+  }
+
+  public onSubmit(): void {
+    console.log(this.myForm.value);
   }
 
 }
